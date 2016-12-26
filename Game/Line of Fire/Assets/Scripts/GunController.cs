2732e9fceabaa032aa1497707b5e32b2c704d8ca
Mr9Madness <Chris241@hotmail.nl>
2016-12-26 04:47:17 +0100

    omdat je moeder dat ook heeft.

---
+++ Game/Line of Fire/Assets/Scripts/GunController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject GunFirePos;
+    LineRenderer LR;
+
+    void Start()
+    {
+        LR = GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+
+        if( !Input.GetMouseButton( 0 ) )
+            return;
+
+        StartCoroutine( shoot() );
+
+        Vector3 rayOrigin = Camera.main.ViewportToWorldPoint( new Vector3( 0.5f, 0.5f ) );
+
+        RaycastHit hit;
+
+        LR.SetPosition( 0, GunFirePos.transform.position );
+
+        if( Physics.Raycast( rayOrigin, Camera.main.transform.forward, out hit, 50f ) )
+        {
+            LR.SetPosition( 1, hit.point );
+        }
+        else
+            LR.SetPosition( 1, rayOrigin + ( Camera.main.transform.forward * 50f ) );
+
+        
+    }
+
+    IEnumerator shoot()
+    {
+        LR.enabled = true;
+        yield return new WaitForSeconds( 0.7f );
+        LR.enabled = false;
+    }
+}
